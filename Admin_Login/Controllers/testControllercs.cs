16168273b3e2333c28242829ab4554976fdc18c7
chrisxk22 <k36_pumas@hotmail.com>
2023-02-24 20:25:16 -0600

    inicio

---
+++ Admin_Login/Controllers/testControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_Login.Controllers
+{
+	public class testControllercs : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
